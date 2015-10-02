1c5cc0991825d33ee6efd8020315cd9f66d0adf2
Nyghtwel <alanliou@Alans-MacBook-Air.local>
2015-10-02 13:09:05 -0400

    added open page function for pin and change default zoom

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,13 +2,20 @@
 
 angular.module('mapnApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.map = { center: { latitude: 29.6485, longitude: -82.345 }, zoom: 1 };
+    $scope.map = { center: { latitude: 29.6485, longitude: -82.345 }, zoom: 10 };
 
     $scope.markers = [{
       id: 0,
       latitude: 29.6485,
       longitude: -82.3450,
-    }];
+    }];    
+    $scope.onClick = function (data) {
+      //URL BLANK AND PARAMENTERS
+        window.open('', '_blank', 'toolbar=0,location=0,menubar=0');
+        console.log('$scope.windowOptions.show: ');
+        //alert('This is a ' + data);
+    };
+
 
     $scope.addMarker = function(marker) {
       // Check that lat/long was input
@@ -18,3 +25,4 @@ angular.module('mapnApp')
       $scope.markers.push(marker);
     }
   });
+  
