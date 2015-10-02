54bfa79aa3db2b5c7912017831fb7b814afded2f
Haralambos <hgogonis@gmail.com>
2015-10-02 17:34:36 -0400

    clicking pin opens a the pin route

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,19 +1,17 @@
 'use strict';
 
 angular.module('mapnApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, $location) {
     $scope.map = { center: { latitude: 29.6485, longitude: -82.345 }, zoom: 10 };
 
     $scope.markers = [{
       id: 0,
       latitude: 29.6485,
       longitude: -82.3450,
-    }];    
-    $scope.onClick = function (data) {
-      //URL BLANK AND PARAMENTERS
-        window.open('', '_blank', 'toolbar=0,location=0,menubar=0');
-        console.log('$scope.windowOptions.show: ');
-        //alert('This is a ' + data);
+    }]; 
+
+    $scope.onClick = function (data, event_name, marker) {
+        $location.path('/pin/' + marker.id);
     };
 
 
