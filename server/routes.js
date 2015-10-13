e6ea4ad818f7475a898f4491ffa8dcb507d9837f
Shawn Chiamchittrong <schiamchittrong@outlook.com>
2015-10-13 14:56:06 -0400

    added routes

--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,7 @@ var path = require('path');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/bounties', require('./api/bounty'));
   app.use('/api/pins', require('./api/pin'));
   app.use('/api/users', require('./api/user'));
   app.use('/api/entries', require('./api/entry'));
