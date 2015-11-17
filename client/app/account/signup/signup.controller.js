b242fb3c153e00ed50166b6cc9b8b128cc3e9043
Haralambos <hgogonis@gmail.com>
2015-11-16 20:22:42 -0500

    changed form validation UI on signup page, recaptcha verification in back-end

--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -1,20 +1,35 @@
 'use strict';
 
 angular.module('mapnApp')
-  .controller('SignupCtrl', function ($scope, Auth, $location, $window) {
+  .controller('SignupCtrl', function ($scope, Auth, $location, $window, vcRecaptchaService) {
     $scope.user = {};
     $scope.errors = {};
+    $scope.recaptcha = {};
+
+    $scope.rcResponse = function (response) {
+      console.log(response);
+      $scope.recaptcha.response = response;
+    }
+
+    $scope.rcCreate = function (id) {
+      $scope.recaptcha.id = id;
+    }
+
+    $scope.rcExpiration = function() {
+      console.info('Captcha expired. Resetting response object');
+      $scope.recaptcha = {};
+   };
 
     $scope.register = function(form) {
       $scope.submitted = true;
-
-      console.log(form);
+      var rc = $scope.recaptcha.response || '';
 
       if(form.$valid) {
         Auth.createUser({
           name: $scope.user.firstname + ' ' +  $scope.user.lastname,
           email: $scope.user.email,
-          password: $scope.user.password
+          password: $scope.user.password,
+          'g-recaptcha-response': rc
         })
         .then( function() {
           // Account created, redirect to home
@@ -23,6 +38,7 @@ angular.module('mapnApp')
         .catch( function(err) {
           err = err.data;
           $scope.errors = {};
+          vcRecaptchaService.reload($scope.recaptcha.id);
 
           // Update validity of form fields that match the mongoose errors
           angular.forEach(err.errors, function(error, field) {
