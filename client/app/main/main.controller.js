2621a8513159894d50ad1213ee98e130fddbe963
nyghtwel <nyghtwel@outlook.com>
2015-11-19 18:04:58 -0500

    able to get location

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,6 +4,11 @@ angular.module('mapnApp')
   .controller('MainCtrl', function ($scope, $http, $location) {
     $scope.map = { center: { latitude: 29.6485, longitude: -82.345 }, zoom: 10 };
     $scope.markers = [];
+    $scope.lat = "0";
+    $scope.lng = "0";
+    $scope.accuracy = "0";
+    $scope.error = "";
+    $scope.danger = false;
     /* my var    */
     
     /* */
@@ -40,6 +45,28 @@ angular.module('mapnApp')
       $scope.markers.push(marker);
     };
 
+    $scope.showPosition = function (position) {
+      $scope.lat = position.coords.latitude;
+      $scope.lng = position.coords.longitude;
+      $scope.accuracy = position.coords.accuracy;
+      //$scope.$apply();
+
+      //var latlng = new google.maps.LatLnt($scope.lat, $scope.lng);
+      //$scope.model.myMap.setCenter(latlng);
+    };
+    $scope.getLocation = function () {
+      
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition($scope.showPosition);
+      }
+      else {
+          $scope.error = "Geolocation is not supported by this browser.";
+      }
+    };
+ 
+    $scope.getLocation();
+    
+
   })
   .controller('controlCtrl', function ($scope){
     $scope.controlText = 'I\'m a custom control';
@@ -54,45 +81,12 @@ angular.module('mapnApp')
         
       }
       else {
+        $scope.map = { center: { latitude: 0, longitude: 0 }, zoom: 1 };
         $scope.error = "geolocation is not supported by this browser.";
         $scope.danger = !$scope.danger;
         alert('custom control clicked!');
       }
       
-    };
-    $scope.getLocation = function () {
-      if(navigator.getlocation){
-        navigator.geolocation.getCurrentPosition($scope.showPosition, $scope.showError);
-      }
-      else {
-        $scope.error = "geolocation is not supported by this browser.";
-      }
-    };
-    $scope.showError = function (error) {
-      switch (error.code) {
-        case error.PERMISSION_DENIED:
-          $scope.error = "User denied the request for Geolocaiton."
-          break;
-        case error.POSITION_UNAVAILABLE:
-          $scope.error = "Location information is unavailable."
-          break;
-        case error.TIMEOUT:
-          $scope.error = "The request to get user locaiton timed out."
-          break;
-        case error.UNKNOWN_ERROR:
-          $scope.error = "An unknown error occurred."
-          break;
-      };
-      $scope.$apply();
-    };
-    $scope.showPosition = function (position) {
-      $scope.lat = position.coords.latitude;
-      $scope.lng = position.coords.longitude;
-      $scope.accuracy = position.coords.accuracy;
-      $scope.$apply();
-
-      var latlng = new google.maps.LatLnt($scope.lat, $scope.lng);
-      $scope.model.myMap.setCenter(latlng);
     };
 
   });
