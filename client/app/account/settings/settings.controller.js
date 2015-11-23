63f38ac408d3e6ea7fd41db621ae257b1aac6087
Haralambos <hgogonis@gmail.com>
2015-11-23 12:58:46 -0500

    updated settings UI, now supports user photo

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -1,21 +1,26 @@
 'use strict';
 
 angular.module('mapnApp')
-  .controller('SettingsCtrl', function ($scope, User, Auth) {
+  .controller('SettingsCtrl', function ($scope, User, Auth, Modal) {
     $scope.errors = {};
+    $scope.user = Auth.getCurrentUser() || {};
 
     $scope.changePassword = function(form) {
       $scope.submitted = true;
       if(form.$valid) {
-        Auth.changePassword( $scope.user.oldPassword, $scope.user.newPassword )
-        .then( function() {
-          $scope.message = 'Password successfully changed.';
-        })
-        .catch( function() {
-          form.password.$setValidity('mongoose', false);
-          $scope.errors.other = 'Incorrect password';
-          $scope.message = '';
-        });
+
+        Modal.confirm.changePassword(function(user) {
+          console.log(user);
+          Auth.changePassword( user.oldPassword, user.newPassword )
+          .then( function() {
+            $scope.message = 'Password successfully changed.';
+          })
+          .catch( function() {
+            form.password.$setValidity('mongoose', false);
+            $scope.errors.other = 'Incorrect password';
+            $scope.message = '';
+          });
+        })($scope.user);
       }
 		};
   });
